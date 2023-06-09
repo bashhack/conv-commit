e5423db90bcd2ec9deaa5787b569b27b920c4b0b
Marc Alvarez <marcalvarez@fastmail.com>
2023-06-08 18:55:58 -0600

    refactor(cli): Conditionally show welcome message
    Only show welcome message if user is in valid git repository,
    otherwise allow exit 1.

--- conv_commit.sh
+++ conv_commit.sh
@@ -5,11 +5,11 @@
 # Conventional Commit spec (https://www.conventionalcommits.org/en/v1.0.0/#specification)
 # and then prompts for the summary and detailed description of the message.
 
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hey friend! Let's $(gum style --foreground 212 'git') committin'."
-
-sleep 1.5; clear
-
 if [ -n "$(git status --porcelain)" ]; then
+  gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hey friend! Let's $(gum style --foreground 212 'git') committin'."
+
+  sleep 1.5; clear
+
   if gum confirm "Stage all?"; then
     git add . && gum spin -s line --title "Staging files..." -- sleep 1
   else
